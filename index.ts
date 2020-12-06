59d67b9270294bd50374b12ccea0d03036391570
Austin McPhail <austin@mcphail.dev>
2020-12-06 15:50:36 -0600

    feat: 🎸 day 3

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { parse } from "https://deno.land/std/flags/mod.ts";
 import { dayOne } from "./d1/d1.ts";
 import { dayTwo } from "./d2/d2.ts";
+import { dayThree } from "./d3/d3.ts";
 
 const DEBUG = parse(Deno.args).d || Boolean(parse(Deno.args).debug);
 
@@ -12,5 +13,8 @@ switch (DAY) {
     break;
   case 2:
     dayTwo(DEBUG, "./d2/data.txt");
+    break;
+  case 3:
+    dayThree(DEBUG, "./d3/data.txt");
     break;
 }
